ce4a7e5d9d764572628ff5a5aaa356919d85fe5b
krishnasaurabh <krishnasaurabh123@gmail.com>
2018-06-02 13:23:13 +0530

    added models

--- IssueMapApp/models.py
+++ IssueMapApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Issue(models.Model):
+    id = models.AutoField(primary_key=True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    description = models.TextField(max_length=500)
+    department = models.CharField(max_length=20)
+    severity = models.IntegerField()
+    creation_date = models.DateField()
+    username = models.ForeignKey(User)
+
+
+    def __str__(self):
+        return self.id
